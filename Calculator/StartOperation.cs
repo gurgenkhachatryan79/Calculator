89d14b6a9c8820ff4b9eda6c81db314c6174d0f8
GurgenKhachatryan <gurgenkhachaatryan79@gmail.com>
2022-02-16 23:42:08 +0400

    add dll

--- Calculator/StartOperation.cs
+++ Calculator/StartOperation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Linq;
 
 namespace Calculator
 {
@@ -12,10 +13,10 @@ namespace Calculator
             string input = null;
             double first = 0;
             string operand = null;
-           
+
             do
             {
-                
+
                 input = Console.ReadLine();
                 char[] inputarray = input.ToCharArray();
                 bool flag = false;
@@ -24,59 +25,63 @@ namespace Calculator
 
                 foreach (var item in inputarray)
                 {
-                    if (item == '=') { break; }   
+                    if (item == '=') { break; }
 
-                        if (item >= '0' && item <= '9')
+                    if (item >= '0' && item <= '9')
+                    {
+                        if (flag == false)
                         {
-                            if (flag == false)
-                            {
-                                if (first != 0) { first = first * 10 + Convert.ToDouble(item.ToString()); }
-                                else { first = Convert.ToDouble(item.ToString()); }
-                            }
-                            else
-                            {
-                                if (second != 0) { second = second * 10 + Convert.ToDouble(item.ToString()); }
-                                else { second = Convert.ToDouble(item.ToString()); }
-
-                            }
+                            if (first != 0) { first = first * 10 + Convert.ToDouble(item.ToString()); }
+                            else { first = Convert.ToDouble(item.ToString()); }
                         }
                         else
-                        { operand = item.ToString(); flag = true; }
-                   
+                        {
+                            if (second != 0) { second = second * 10 + Convert.ToDouble(item.ToString()); }
+                            else { second = Convert.ToDouble(item.ToString()); }
+
+                        }
+                    }
+                    else
+                    { operand = item.ToString(); flag = true; }
+
                 }
-                Func<string, IOperation> func=null;
+                Func<string, IOperation> func = null;
                 //   List<IOperation> operations=null;
                 //   var operation= GetOperation(operand);
                 //    operations.Add(operation);     
                 func += GetOperation;
                 Calculator calculator = new Calculator(func);
-                first=calculator.Calculate(operand, first, second);
+                first = calculator.Calculate(operand, first, second);
             }
             while (input != "e");
-           
+
         }
-      
+
         public static IOperation GetOperation(string operand)
         {
             Assembly assembly = null;
+            IOperation instance = null;
             try
-            { 
+            {
                 assembly = Assembly.LoadFrom(@"C:\Users\Toshiba\Desktop\CalculatorAddition\ModulOperation\ModulOperation\bin\Debug\net5.0\ModulOperation.dll");
+                //  assembly = Assembly.LoadFrom("..//..//..//net5.0//ModulOperation.dll");
+
                 Console.WriteLine("dll is created");
                 Type type = assembly.GetType("ModulOperation.ModOperation");
                 Console.WriteLine("type=" + type);
 
-                object instance = Activator.CreateInstance(type) as IOperation;
+                instance = Activator.CreateInstance(type) as IOperation;
+
+
+                // instance = new ModOperation();
                 if (instance == null) { Console.WriteLine("instance is null"); }
-               
-              
+                else { Console.WriteLine(" instance not null"); }
+
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
-
-
 
 
             switch (operand)
@@ -99,8 +104,7 @@ namespace Calculator
                     }
                 case "%":
                     {
-                       // Console.WriteLine(type.GetMethod("ModulOpperation").Name);
-                        //  return new Activator.CreateInstance(type) as IOperation; 
+                        return instance;
                         break;
                     }
                 default:
